e4b7643811df887608d8cc77df569849bc385c33
Amir Tadrisi <amirtds@gmail.com>
2024-05-15 18:36:18 -0500

    init

---
+++ src/components/Solution.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Solution = () => {
+  return (
+    <Image
+      src="https://i.ibb.co/YWKzRJT/Screenshot-2024-05-15-at-4-49-46-PM.png"
+      alt=""
+      className=""
+      width={800}
+      height={1200}
+      unoptimized
+    />
+  )
+}
+
+export default Solution
